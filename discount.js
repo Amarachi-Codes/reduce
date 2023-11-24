382b540bce69e5cf13096b6292eb04cd6e9a645b
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-24 18:17:28 +0100

    discount

---
+++ discount.js
@@ -0,0 +1,43 @@
+// Discount Rules
+// customers who have purchased more than 4 products gets a 10% discount.
+// customers who have spent more than $200 gets a $25 discount
+// Kindly write code to apply discount rules
+// Best of Luck
+
+let customers = [
+    {
+        id:1,
+        name: 'Alice',
+        totalAmountSpent: 150,
+        totalProductsPurchased: 5
+    },
+    {
+        id:2,
+        name: 'Bob',
+        totalAmountSpent: 100,
+        totalProductsPurchased: 3
+    },
+    {
+        id:3,
+        name: 'Charlie',
+        totalAmountSpent: 300,
+        totalProductsPurchased: 8
+    }
+];
+
+
+const getDiscount = (customers)=>{
+    let moreThan4 = 4;
+    let tenPercentDiscount = 0.1;
+    let moreThan200 = 200;
+    let gets25Discount = 25;
+    return customers.map((customer)=>{
+        let discount = 0;
+        if(customer.totalProductsPurchased > moreThan4){
+            discount+=tenPercentDiscount * customer.totalAmountSpent
+        }if(customer.totalAmountSpent > moreThan200){
+            discount+=gets25Discount
+        } return {...customer, discountGiven: discount}
+    })
+}
+console.log(getDiscount(customers));
