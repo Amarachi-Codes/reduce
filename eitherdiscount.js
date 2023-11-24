2033c5e09bafb8e0c18dcc712782cc7325ec1c14
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-24 18:34:36 +0100

    updated succesfully

--- eitherdiscount.js
+++ eitherdiscount.js
@@ -39,3 +39,21 @@ const getDiscount = (customers)=>{
     })
 }
 console.log(getDiscount(customers));
+
+//or
+
+const getDiscounts = (customers)=>{
+    let moreThan4 = 4;
+    let tenPercentDiscount = 0.1;
+    let moreThan200 = 200;
+    let gets25Discount = 25;
+    return customers.map((customer)=>{
+        let discount = 0;
+        if(customer.totalProductsPurchased > moreThan4){
+            discount=tenPercentDiscount * customer.totalAmountSpent
+        }else if(customer.totalAmountSpent > moreThan200){
+            discount=gets25Discount
+        } return {...customer, discountGiven: discount}
+    })
+}
+console.log(getDiscounts(customers));
